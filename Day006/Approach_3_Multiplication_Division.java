860412955c22b7b8ba9e6b8a37100c07bffa0710
mraniket404 <aniketgosavi471@gmail.com>
2026-06-20 23:34:43 +0530

    Approach_3_Multiplication_Division

---
+++ Day006/Approach_3_Multiplication_Division.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Approach_3_Multiplication_Division {
+    public static void swip_number(int num1 , int num2) {
+        num1 *= num2;
+        num2 =num1 / num2;
+        num1 /= num2;
+        System.out.println(num1);
+        System.out.println(num2);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter the number : ");
+        int n2 = sc.nextInt();
+
+        swip_number(n1,n2);
+    }
+}
