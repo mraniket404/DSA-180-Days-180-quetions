d5a105d683605e23a9548ee244d75cb77d7a2450
mraniket404 <aniketgosavi471@gmail.com>
2026-06-20 23:26:08 +0530

    Approach_2_Without_Third_Variable

--- Day006/Approach_2_Without_Third_Variable.java
+++ Day006/Approach_2_Without_Third_Variable.java
@@ -1,5 +1,19 @@
-package Day006;
-
+import java.util.*;
 public class Approach_2_Without_Third_Variable {
-    
+    public static void swip_number(int num1 , int num2) {
+        num1 += num2;
+        num2=num1-num2;
+        num1-=num2;
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter the number : ");
+        int n2 = sc.nextInt();
+
+        swip_number(n1,n2);
+    }
 }
