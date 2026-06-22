0a30ea99e0872394276644477f8dd9f7abdb1a3d
mraniket404 <aniketgosavi471@gmail.com>
2026-06-22 11:53:36 +0530

    Approach_1_Using_For_Loop

---
+++ Day008/Approach_1_Using_For_Loop.java
@@ -0,0 +1,30 @@
+// Day 8 - Factorial of a Number 
+// Question 
+// Ek number N diya gaya hai. Uska factorial find karo. 
+// Formula 
+// n! = n × (n-1) × (n-2) × ... × 1 
+// Example 
+// 5! = 5 × 4 × 3 × 2 × 1 
+ 
+// = 120 
+
+
+import java.util.*;
+public class Approach_1_Using_For_Loop {
+    public static void factorial(int num) {
+        long fact = 1;
+        for (int i = 1;i<=num;i++){
+            fact*=i;
+        }
+        System.out.println(num+ " of factorial is "+fact);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        factorial(n);
+
+
+    }
+}
